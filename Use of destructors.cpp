96482cb677e5c42194bf8a59c3998527a28d881d
Mayank Yadav <mayankyadav18101@gmail.com>
2025-09-02 01:36:04 +0530

    Create Use of destructors.cpp

---
+++ Use of destructors.cpp
@@ -0,0 +1,38 @@
+/*Name - Mayank yadav
+  PRN - 24070123060
+  Class - ENTC A-3
+  Date - 28/8/2025 */
+#include <iostream>
+using namespace std;
+
+int count = 0;
+class destruct{
+    public:
+    destruct(){
+        count++;
+        cout << "No. of objects created:" << count << endl;
+    }
+    ~destruct(){
+        count--;
+        cout << "No. of objects destroyed:" << count << endl;
+    }
+};
+
+int main(){
+    destruct aa,bb,cc,dd;
+
+    return 0;
+}
+
+
+/*OUTPUT
+No. of objects created:1
+No. of objects created:2
+No. of objects created:3
+No. of objects created:4
+No. of objects destroyed:3
+No. of objects destroyed:2
+No. of objects destroyed:1
+No. of objects destroyed:0
+
+  */
