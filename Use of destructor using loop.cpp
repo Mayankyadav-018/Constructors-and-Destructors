f047e77895156aebebfe4f3d9ed0164464f62ac8
Mayank Yadav <mayankyadav18101@gmail.com>
2025-09-02 01:38:09 +0530

    Create Use of destructor using loop.cpp

---
+++ Use of destructor using loop.cpp
@@ -0,0 +1,37 @@
+/*Name - Mayank yadav
+  PRN - 24070123060
+  Class - ENTC A-3
+  Date - 28/8/2025 */
+#include <iostream>
+using namespace std;
+class Date{
+    public:
+    int d = 26;
+    int m = 8;
+    int y = 2025;
+    
+    ~Date(){
+        cout << "Destructor called" << endl;
+    }
+};
+int main(){
+    Date d1,d2,d3,d4;
+    int i;
+    for(i=0;i<4;i++){
+        Date i;
+    }
+    return 0;
+}
+
+
+/*OUTPUT
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+Destructor called
+
+  */
